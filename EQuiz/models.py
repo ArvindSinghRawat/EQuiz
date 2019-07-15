8441b1c275f016b609cc98209e0a415fddb166ea
ArvindSinghRawat <arvid160102261@gmail.com>
2019-07-15 19:06:14 +0530

    Added Models and Cosmetic Changes
    Added Quiz Model
    Changed Main View

--- EQuiz/models.py
+++ EQuiz/models.py
@@ -12,16 +12,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Organisation(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
-    name = models.CharField(db_column='Name', max_length=129)  # Field name made lowercase.
-    country = models.CharField(db_column='Country', max_length=32, blank=True, null=True)  # Field name made lowercase.
-    address = models.CharField(db_column='Address', max_length=257, blank=True, null=True)  # Field name made lowercase.
-    representative = models.CharField(db_column='Representative', max_length=129)  # Field name made lowercase.
-    mobile = models.CharField(db_column='Mobile', max_length=15, blank=True, null=True)  # Field name made lowercase.
-    email = models.CharField(db_column='Email', max_length=129, blank=True, null=True)  # Field name made lowercase.
-    type = models.CharField(db_column='Type', max_length=129)  # Field name made lowercase.
-    createdon = models.DateField(db_column='CreatedOn', blank=True, null=True)  # Field name made lowercase.
-    creationmode = models.CharField(db_column='CreationMode', max_length=45, default="Website",editable=False)  # Field name made lowercase.
+    id = models.AutoField(db_column='Id', primary_key=True)  
+    name = models.CharField(db_column='Name', max_length=129)  
+    country = models.CharField(db_column='Country', max_length=32, blank=True, null=True)  
+    address = models.CharField(db_column='Address', max_length=257, blank=True, null=True)  
+    representative = models.CharField(db_column='Representative', max_length=129)  
+    mobile = models.CharField(db_column='Mobile', max_length=15, blank=True, null=True)  
+    email = models.CharField(db_column='Email', max_length=129, blank=True, null=True)  
+    type = models.CharField(db_column='Type', max_length=129)  
+    createdon = models.DateField(db_column='CreatedOn', blank=True, null=True)  
+    creationmode = models.CharField(db_column='CreationMode', max_length=45, default="Website",editable=False)  
     
     def __str__(self):
         return self.name
@@ -31,29 +31,29 @@ class Organisation(models.Model):
         db_table = 'Organisation'
 
 class AdminUser(models.Model):
-    # Field name made lowercase.
+    
     name = models.CharField(db_column='UserName', unique=True, max_length=129)
-    # Field name made lowercase.
+    
     password = models.TextField(db_column='Password')
-    # Field name made lowercase.
+    
     organisationid = models.ForeignKey(
         'Organisation', models.DO_NOTHING, db_column='Organisation')
-    # Field name made lowercase.
+    
     country = models.CharField(
         db_column='Country', max_length=33, blank=True, null=True)
-    # Field name made lowercase.
+    
     address = models.CharField(
         db_column='Address', max_length=257, blank=True, null=True)
-    type = models.IntegerField(db_column='Type', default=1)  # Field name made lowercase.
-    # Field name made lowercase.
+    type = models.IntegerField(db_column='Type', default=1)  
+    
     createdon = models.DateTimeField(
         db_column='CreatedOn', blank=True, null=True)
-    # Field name made lowercase.
+    
     creationmode = models.CharField(db_column='CreationMode', max_length=17)
-    # Field name made lowercase.
+    
     mobile = models.CharField(
         db_column='Mobile', max_length=13, blank=True, null=True)
-    # Field name made lowercase.
+    
     email = models.CharField(
         db_column='Email', max_length=129, blank=True, null=True)
 
@@ -61,27 +61,28 @@ class AdminUser(models.Model):
         managed = True
         db_table = 'AdminUsers'
 
+    def __str__(self):
+        return self.name+" : "+self.email
+
 class Question(models.Model):
-    id = models.BigAutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    question = models.CharField(db_column='Question', max_length=512)  # Field name made lowercase.
-    option1 = models.CharField(db_column='Option1', max_length=512)  # Field name made lowercase.
-    option2 = models.CharField(db_column='Option2', max_length=512)  # Field name made lowercase.
-    option3 = models.CharField(db_column='Option3', max_length=512, blank=True, null=True)  # Field name made lowercase.
-    option4 = models.CharField(db_column='Option4', max_length=512, blank=True, null=True)  # Field name made lowercase.
-    answeroption1 = models.IntegerField(db_column='AnswerOption1')  # Field name made lowercase.
-    answeroption2 = models.IntegerField(db_column='AnswerOption2')  # Field name made lowercase.
-    answeroption3 = models.IntegerField(db_column='AnswerOption3', blank=True, null=True)  # Field name made lowercase.
-    answeroption4 = models.IntegerField(db_column='AnswerOption4', blank=True, null=True)  # Field name made lowercase.
-    type = models.ForeignKey('Questiontype', models.DO_NOTHING, db_column='Type')  # Field name made lowercase.
-    difficulty = models.IntegerField(db_column='Difficulty')  # Field name made lowercase.
-    attachments = models.CharField(db_column='Attachments', max_length=2024, blank=True, null=True)  # Field name made lowercase.
-    madeby = models.ForeignKey('AdminUser', models.CASCADE, db_column='MadeBy', blank=True, null=True)  # Field name made lowercase.
-    createdon = models.DateField(db_column='CreatedOn', blank=True, null=True)  # Field name made lowercase.
-    lastupdated = models.DateTimeField(db_column='LastUpdated', blank=True, null=True,editable=False,default=datetime.now())  # Field name made lowercase.
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
+    id = models.BigAutoField(db_column='ID', primary_key=True)  
+    question = models.CharField(db_column='Question', max_length=512)  
+    option1 = models.CharField(db_column='Option1', max_length=512)  
+    option2 = models.CharField(db_column='Option2', max_length=512)  
+    option3 = models.CharField(db_column='Option3', max_length=512, blank=True, null=True)  
+    option4 = models.CharField(db_column='Option4', max_length=512, blank=True, null=True)  
+    answeroption1 = models.IntegerField(db_column='AnswerOption1')  
+    answeroption2 = models.IntegerField(db_column='AnswerOption2')  
+    answeroption3 = models.IntegerField(db_column='AnswerOption3', blank=True, null=True)  
+    answeroption4 = models.IntegerField(db_column='AnswerOption4', blank=True, null=True)  
+    type = models.ForeignKey('Questiontype', models.DO_NOTHING, db_column='Type')  
+    difficulty = models.IntegerField(db_column='Difficulty')  
+    attachments = models.CharField(db_column='Attachments', max_length=2024, blank=True, null=True)  
+    madeby = models.ForeignKey('AdminUser', models.CASCADE, db_column='MadeBy', blank=True, null=True)  
+    createdon = models.DateField(db_column='CreatedOn', blank=True, null=True)  
+    lastupdated = models.DateTimeField(db_column='LastUpdated', blank=True, null=True,editable=False,default=datetime.now()) 
+    quizassociated = models.ForeignKey('Quiz', models.CASCADE, db_column='QuizAssociated', blank=True, null=True)
+
     class Meta:
         managed = True
         db_table = 'Question'
@@ -96,16 +97,16 @@ class Questiontype(models.Model):
         ('NUM', 'Number'),
         ('TXT', 'Text'),
     ]
-    id = models.BigAutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    description = models.CharField(db_column='Description', max_length=512, blank=True, null=True)  # Field name made lowercase.
-    code = models.CharField(db_column='Code', max_length=2042)  # Field name made lowercase.
-    optionsrequired = models.IntegerField(db_column='OptionsRequired')  # Field name made lowercase.
-    questionimage = models.IntegerField(db_column='QuestionImage', blank=True, null=True,default=0)  # Field name made lowercase.
-    answerimage = models.IntegerField(db_column='AnswerImage', blank=True, null=True,default=0)  # Field name made lowercase.
-    type = models.CharField(db_column='Type', max_length=3, blank=True, null=True,choices=Question_types)  # Field name made lowercase.
-    madeby = models.ForeignKey('AdminUser', models.CASCADE, db_column='MadeBy', blank=True, null=True)  # Field name made lowercase.
-    createdon = models.DateTimeField(db_column='CreatedOn', blank=True, null=True)  # Field name made lowercase.
-    lastupdated = models.DateTimeField(db_column='LastUpdated', blank=True, null=True,editable=False,default=datetime.now())  # Field name made lowercase.
+    id = models.BigAutoField(db_column='ID', primary_key=True)  
+    description = models.CharField(db_column='Description', max_length=512, blank=True, null=True)  
+    code = models.CharField(db_column='Code', max_length=2042)  
+    optionsrequired = models.IntegerField(db_column='OptionsRequired')  
+    questionimage = models.IntegerField(db_column='QuestionImage', blank=True, null=True,default=0)  
+    answerimage = models.IntegerField(db_column='AnswerImage', blank=True, null=True,default=0)  
+    type = models.CharField(db_column='Type', max_length=3, blank=True, null=True,choices=Question_types)  
+    madeby = models.ForeignKey('AdminUser', models.CASCADE, db_column='MadeBy', blank=True, null=True)  
+    createdon = models.DateTimeField(db_column='CreatedOn', blank=True, null=True)  
+    lastupdated = models.DateTimeField(db_column='LastUpdated', blank=True, null=True,editable=False,default=datetime.now())  
 
     class Meta:
         managed = True
@@ -113,3 +114,20 @@ class Questiontype(models.Model):
 
     def __str__(self):
         return self.type+" "+self.description
+
+class Quiz(models.Model):
+    id = models.BigAutoField(db_column='Id', primary_key=True)
+    name = models.CharField(db_column='Name', max_length=129)
+    numberofquestions = models.IntegerField(db_column='NumberOfQuestions', blank=True)
+    duration = models.BigIntegerField(db_column='Duration', blank=True, null=True)
+    organisation = models.ForeignKey(Organisation, models.CASCADE, db_column='Organisation')
+    positivemarks = models.IntegerField(db_column='PositiveMarks',default=1)
+    negativemarks = models.IntegerField(db_column='NegativeMarks',default=0)
+    description = models.CharField(db_column='Description', max_length=1025, blank=True, null=True,default='')
+    difficultyratio = models.CharField(db_column='DifficultyRatio', max_length=64, blank=True, null=True,default='')
+    createdby = models.ForeignKey('AdminUser', models.CASCADE, db_column='CreatedBy', blank=True, null=True)
+    createdon = models.DateTimeField(db_column='CreatedOn', blank=True, null=True,default=datetime.now())
+
+    class Meta:
+        managed = True
+        db_table = 'Quiz'
